b32035d2a4bc9aa299b62d11c92f8d17a30b56a0
arv-raj <arajan136@gmail.com>
2019-08-25 04:44:36 -0700

    Updated Files

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var port = 8080;
 app.listen(8080, function () {
-	console.log('IMAD course app listening on  port ${port}!');
+	console.log('IMAD course app listening on port',port,'!');
 });

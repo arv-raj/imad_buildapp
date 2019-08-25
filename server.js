e41dde67316e2fe57857032430c49543018e757e
arv-raj <arajan136@gmail.com>
2019-08-25 06:25:10 -0700

    Update_08-25-2019

--- server.js
+++ server.js
@@ -5,8 +5,91 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one': {
+		title: 'Article One ! IMAD App',
+		heading: 'Article One',
+		date: '08-25-2019',
+		content: `
+				<p>
+					Let's say this is the begining of something new. Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>
+				<p>
+					Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>`
+	},
+	'article-two': {
+		title: 'Article Two ! IMAD App',
+		heading: 'Article Two',
+		date: '08-25-2019',
+		content: `
+				<p>
+					Let's say this is the begining of something new. Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>
+				<p>
+					Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>`
+	},
+	'article-three': {
+		title: 'Article Three ! IMAD App',
+		heading: 'Article Three',
+		date: '08-25-2019',
+		content: `
+				<p>
+					Let's say this is the begining of something new. Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>
+				<p>
+					Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.Let's say this is the begining of something new.
+				</p>`
+	}
+};
+
+function createTemplate (data) {
+	var title = data.title;
+	var date = data.date;
+	var heading = data.heading;
+	var content = data.content;
+	var htmlTemplate = `
+	<html>
+		<head>
+			<title>
+				${title}
+			</title>
+			<meta name="viewport" content="width=device-width, initia-scale=1"/>
+			<link href="/ui/style.css" rel="stylesheet"/>
+		</head>
+		<body>
+			<div class="container">
+				<div>
+					<a href="/">Home</a>
+				</div>
+				<hr/>
+				<h3>
+					${heading}
+				</h3>
+				<div>
+					${date}
+				</div>
+				<div>
+					${content}     
+				</div>
+			</div>
+		</body>
+	</html>
+	`;
+	return htmlTemplate;
+}
+
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function(req, res) {
+	// articleName = article-one
+	// articles[articleName] == {} content object for article one
+	var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
+	/*res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));*/
 });
 
 app.get('/ui/style.css', function (req, res) {
